7e59be987e98b9be1a2886620fba093eb251143b
Aklima-Shampa <shampa.iub@gmail.com>
2024-03-11 00:35:21 +0600

    added dummy text in FeatureCard

--- components/FeatureCard/index.js
+++ components/FeatureCard/index.js
@@ -5,7 +5,7 @@ const FeatureCard = () => {
     return (
         <div className={styles.cardContainer}>
             <h2>Hello I am card</h2>
-            <p></p>
+            <p>Lorem ipsum dolor sit amet.</p>
         </div>
     )
 }
