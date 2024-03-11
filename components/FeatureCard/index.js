13071f5a860f9d570a42f49579480f66a6865190
Aklima-Shampa <shampa.iub@gmail.com>
2024-03-11 10:35:37 +0600

    style added to FeatureCard

--- components/FeatureCard/index.js
+++ components/FeatureCard/index.js
@@ -1,14 +1,30 @@
 import React from 'react'
 import styles from "./card.module.scss"
+import { HiBars4 } from "react-icons/hi2";
+import { FaArrowRightLong } from "react-icons/fa6";
 
 const FeatureCard = () => {
     return (
         <div className={styles.cardContainer}>
-            <h2>Hello I am card</h2>
-            <p>Lorem ipsum dolor sit amet.</p>
+            <HiBars4 />
+            <h2>Business Stratagy</h2>
+            <p>I throw myself down among the tall grass by the stream as Ilie close to the earth.</p>
+            <FaArrowRightLong />
         </div>
     )
 }
+
+
+
+// const FeatureCard = () => {
+//     return (
+//         <div  className={styles.cardContainer}>
+
+            // <h2>Business Stratagy</h2>
+            // <p>I throw myself down among the tall grass by the stream as Ilie close to the earth.</p>
+//         </div>
+//     );
+// };
 
 
 export default FeatureCard
