360cfd6de291b47dd093237c3646fd607868fc4a
Aklima-Shampa <shampa.iub@gmail.com>
2024-03-19 22:33:07 +0600

    heroSection edited

--- app/page.js
+++ app/page.js
@@ -1,11 +1,13 @@
 import Header from '@/components/Header';
 import FeatureSection from '@/section/FeatureSection';
+import HeroSection from '@/section/HeroSection';
 import PortfolioSection from '@/section/PortfolioSection';
 
 export default function Home() {
   return (
     <main>
       <Header />
+      <HeroSection/>
       <FeatureSection />
       <PortfolioSection/>
     </main>
