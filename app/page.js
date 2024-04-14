eeb413b09f7b06b302d567f3ee55bf122a679e22
Aklima-Shampa <shampa.iub@gmail.com>
2024-04-15 02:30:52 +0600

    Tab component added

--- app/page.js
+++ app/page.js
@@ -1,10 +1,11 @@
 import Header from '@/components/Header';
+import ResumeTabs from '@/components/ResumeTabs';
+
 import Testimonial from '@/components/Testimonial/Testimonial';
 import BlogCardSection from '@/section/BlogCardSection';
 import FeatureSection from '@/section/FeatureSection';
 import HeroSection from '@/section/HeroSection';
 import PortfolioSection from '@/section/PortfolioSection';
-
 
 export default function Home() {
   return (
@@ -15,6 +16,7 @@ export default function Home() {
       <PortfolioSection />
       <BlogCardSection />
       <Testimonial/>
+    <ResumeTabs/>
       
       
     </main>
